731b65c6675999bd1137858f3928c3edf2828a84
myHottHarry <myhottharry@gmail.com>
2021-11-06 23:55:29 -0700

    change

--- README.md
+++ README.md
@@ -13,8 +13,20 @@ Note, this is a long assignment, meant to be completed over the two weeks when w
 You should start this assignment by opening up your `analysis.R` script. The script will guide you through an initial analysis of the data. Throughout the script, there are prompts labeled **Reflection**. Please write 1 - 2 sentences for each of these reflections below:
 
 - What does each row in the data represent (hint: read the [documentation](https://github.com/nytimes/covid-19-data/)!)?
+    - national: Each row represents Covid-19 data of US on a specfic day.
+    - states: Each row represents Covid-19 data of one state on a specfic day.
+    - counties: Each row represents Covid-19 data of one county (in a state) on a specific day.
 - What did you learn about the dataset when you calculated the state with the lowest cases (and what does that tell you about testing your assumptions in a dataset)?
+  - This Covid cases data in this dataset may not be very accurate for every state. For example, the number of cases in American Samoa stays at 1, which is very suspicious.
+  - When it comes to testing assumptions in a dataset, it might be better to drop some extreme value to make our analysis result more reasonable.
 - Is the location with the highest number of cases the location with the most deaths? If not, why do you believe that may be the case? 
+  - No, the location with the highest number of cases is not the location with the most deaths.
+  - Possible Reasons:
+    - The dataset has problem, e.g., miscount the number of cases, miscount the number of deaths.
+    - There exists difference between medical facilities among different locations. Maybe the location with the highest number of cases has better treating facilities, so with more effective treatment, number of deaths at this locations gets better control.
 - What do the plots of cases and deaths tell us about the  pandemic happening in "waves"? How (and why, do you think) these plots look so different?
+  - The number of cases and the number of deaths increase / decrease periodically instead of linearly, so we cannot relax the distancing policy casually just because of a small decreasing tendency in cases / deaths.
+  - The main difference between two plots is that the wave in deaths plot normally reaches peak later than that in cases plot. One possible reason is that when new cases appear, their sympotom will not become the worst (death) in the beginning, so this can explain the delay of time reaching peak.
 - Why are there so many observations (counties) in the variable `lowest_in_each_state` (i.e., wouldn't you expect the number to be ~50)?
+  - It is possible that there are some counties / locations with the same number of cases / deaths. Thus, with further conditions, those counties in the same state with the same number will be returned together.
 - What surprised you the most throughout your analysis?
--- analysis.R
+++ analysis.R
@@ -17,29 +17,34 @@
 # each question (though feel free to ask if it's unclear!)
 
 # Load the tidyverse package
-
+install.packages("tidyverse")
+library(tidyverse)
 
 # Load the *national level* data into a variable. `national`
 # (hint: you'll need to get the "raw" URL from the NYT GitHub page)
-
+national <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
 
 # Load the *state level* data into a variable. `states`
-
+states <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
 # Load the *county level* data into a variable. `counties`
 # (this is a large dataset, which may take ~30 seconds to load)
-
+counties <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 
 # How many observations (rows) are in each dataset?
 # Create `obs_national`, `obs_states`, `obs_counties`
-
+obs_nation <- nrow(national)
+obs_states <- nrow(states)
+obs_counties <- nrow(counties)
 
 # Reflection: What does each row represent in each dataset?
 
 
 # How many features (columns) are there in each dataset?
 # Create `num_features_national`, `num_features_states`, `num_features_counties`
-
+num_features_national <- ncol(national)
+num_features_states <- ncol(states)
+num_features_counties <- ncol(counties)
 
 
 # Exploratory analysis ----------------------------------------------------
@@ -54,27 +59,50 @@
 
 # How many total cases have there been in the U.S. by the most recent date
 # in the dataset? `total_us_cases`
+total_us_cases <- national %>%
+  filter(date == max(date)) %>%
+  pull(cases)
 
 # How many total deaths have there been in the U.S. by the most recent date
 # in the dataset? `total_us_deaths`
+total_us_deaths <- national %>%
+  filter(date == max(date)) %>%
+  pull(deaths)
 
 # Which state has had the highest number of cases?
 # `state_highest_cases`
-
+state_highest_cases <- states %>%
+  group_by(state) %>%
+  summarize(total_cases = max(cases, na.rm = TRUE)) %>%
+  filter(total_cases == max(total_cases)) %>%
+  pull(state)
 
 # What is the highest number of cases in a state?
 # `num_highest_state`
-
+num_highest_state <- states %>%
+  group_by(state) %>%
+  summarize(total_cases = max(cases, na.rm = TRUE)) %>%
+  filter(total_cases == max(total_cases)) %>%
+  pull(total_cases)
 
 # Which state has the highest ratio of deaths to cases (deaths/cases), as of the
 # most recent date? `state_highest_ratio`
 # (hint: you may need to create a new column in order to do this!)
+state_highest_ratio <- states %>%
+  group_by(state) %>%
+  summarize(ratio = max(deaths, na.rm = TRUE) / max(cases, na.rm = TRUE)) %>%
+  filter(ratio == max(ratio)) %>%
+  pull(state)
 
 
 # Which state has had the lowest number of cases *as of the most recent date*?
 # (hint, this is a little trickier to calculate than the maximum because
 # of the meaning of the row). `state_lowest_cases`
-
+state_lowest_cases <- states %>%
+  group_by(state) %>%
+  summarize(total_cases = max(cases, na.rm = TRUE)) %>%
+  filter(total_cases == min(total_cases)) %>%
+  pull(state)
 
 # Reflection: What did you learn about the dataset when you calculated
 # the state with the lowest cases (and what does that tell you about
@@ -82,11 +110,19 @@
 
 # Which county has had the highest number of cases?
 # `county_highest_cases`
-
+county_highest_cases <- counties %>%
+  group_by(county) %>%
+  summarize(total_cases = max(cases, na.rm=TRUE)) %>%
+  filter(total_cases == max(total_cases)) %>%
+  pull(county)
 
 # What is the highest number of cases that have happened in a single county?
 # `num_highest_cases_county`
-
+num_highest_cases_county <- counties %>%
+  group_by(county) %>%
+  summarize(total_cases = max(cases, na.rm=TRUE)) %>%
+  filter(total_cases == max(total_cases)) %>%
+  pull(total_cases)
 
 # Because there are multiple counties with the same name across states, it
 # will be helpful to have a column that stores the county and state together
@@ -95,11 +131,15 @@
 # that stores the county and state (separated by a comma and space).
 # You can do this by mutating a new column, or using the `unite()` function
 # (just make sure to keep the original columns as well)
-
+counties = unite(counties, location, county, state, sep=",", remove=FALSE)
 
 # What is the name of the location (county, state) with the highest number
 # of deaths? `location_most_deaths`
-
+location_most_deaths <- counties %>%
+  group_by(location) %>%
+  summarize(total_deaths = max(deaths, na.rm = TRUE)) %>%
+  filter(total_deaths == max(total_deaths)) %>%
+  pull(location)
 
 # Reflection: Is the location with the highest number of cases the location with
 # the most deaths? If not, why do you believe that may be the case?
@@ -110,31 +150,37 @@
 # Add (mutate) a new column on your `national` data frame called `new_cases`
 # that has the nubmer of *new* cases each day (hint: look for the `lag`
 # function).
-
+national <- mutate(national, new_cases = cases - lag(cases))
 
 # Similarly, the `deaths` columns *is not* the nubmber of new deaths per day.
 # Add (mutate) a new column on your `national` data frame called `new_deaths`
 # that has the nubmer of *new* deaths each day
-
+national <- mutate(national, new_deaths = deaths - lag(deaths))
 
 # What was the date when the most new cases occured?
 # `date_most_cases`
-
+date_most_cases <- national %>%
+  filter(new_cases == max(new_cases, na.rm = TRUE)) %>%
+  pull(date)
 
 # What was the date when the most new deaths occured?
 # `date_most_deaths`
-
+date_most_deaths <- national %>%
+  filter(new_deaths == max(new_deaths, na.rm = TRUE)) %>%
+  pull(date)
 
 # How many people died on the date when the most deaths occured? `most_deaths`
-
+most_deaths <- national %>%
+  filter(new_deaths == max(new_deaths, na.rm = TRUE)) %>%
+  pull(new_deaths)
 
 # Create a (very basic) plot by passing `national$new_cases` column to the
 # `plot()` function. Store the result in a variable `new_cases_plot`.
-
+new_cases_plot <- plot(national$new_cases)
 
 # Create a (very basic) plot by passing the `new_deaths` column to the
 # `plot()` function. Store the result in a variable `new_deaths_plot`.
-
+new_deaths_plot <- plot(national$new_deaths)
 
 # Reflection: what do the plots of new cases and deaths tell us about the
 # pandemic happening in "waves"? How (and why, do you think) these plots
@@ -151,19 +197,30 @@
 # `highest_in_each_state`, should be a *vector* of
 # `location` names (the column with COUNTY, STATE).
 # Hint: be careful about the order of filtering your data!
-
+highest_in_each_state <- counties %>%
+  group_by(state) %>%
+  filter(cases == max(cases, na.rm = TRUE)) %>%
+  pull(location)
+highest_in_each_state <- unique(highest_in_each_state)
 
 # Which locaiton (COUNTY, STATE) has had the highest number of cases
 # in Washington? `highest_in_wa`
 # (hint: you may need to find a match of a particular *string*, and you may
 # just want to use base R syntax rather than a dplyr function)
-
+highest_in_wa <- counties %>%
+  filter(state == "Washington") %>%
+  filter(cases == max(cases, na.rm = TRUE)) %>%
+  pull(location)
 
 # What is the county with the *current* (e.g., on the most recent date)
 # lowest number of deaths in each state? Your answer, stored in
 # `lowest_in_each_state`, should be a *vector* of
 # `location` names (the column with COUNTY, STATE).
-
+lowest_in_each_state <- counties %>%
+  group_by(state) %>%
+  filter(date == max(date, na.rm = TRUE)) %>%
+  filter(deaths == min(deaths, na.rm = TRUE)) %>%
+  pull(location)
 
 # Reflection: Why are there so many observations (counties) in the variable
 # `lowest_in_each_state` (i.e., wouldn't you expect the number to be ~50)?
@@ -175,7 +232,10 @@
 # with both the state name, and the proportion (`prop`) in a variable
 # called `prop_no_deaths`
 # (this one is tricky.... take your time)
-
+num_counties <- group_by(counties, state)
+num_counties <- summarise(num_counties, num_coun=count(location))
+no_death <- group_by(counties, state)
+no_death <- summarise(num_no_death = count())
 
 # What proportion of counties in Washington have had zero deaths?
 # `wa_prop_no_deaths`
@@ -190,29 +250,37 @@
 # `states` dataframe. For clarity, let's call the column with the total cases
 # `state_total`
 # This will be a dataframe with the columns `date` and `state_total`.
-
+state_by_day <- states %>%
+  group_by(date) %>%
+  summarise(state_total = sum(cases))
 
 # Next, let's create `county_by_day` by adding up the cases on each day in the
 # `counties` dataframe. For clarity, let's call the column with the total cases
 # `county_total`
 # This will also be a dataframe, with the columns `date` and `county_total`.
-
+county_by_day <- counties %>%
+  group_by(date) %>%
+  summarise(county_total = sum(cases))
 
 # Now, there are a few ways to check if they are always equal. To start,
 # let's *join* those two dataframes into one called `totals_by_day`
-
+totals_by_day <- left_join(state_by_day, county_by_day, by = "date")
 
 # Next, let's create a variable `all_totals` by joining `totals_by_day`
 # to the `national` dataframe
-
+all_totals <- left_join(national, totals_by_day, by = "date")
 
 # How many rows are there where the state total *doesn't equal* the natinal
 # cases reported? `num_state_diff`
-
+num_state_diff <- all_totals %>%
+  filter(state_total != cases)
+num_state_diff <- nrow(num_state_diff)
 
 # How many rows are there where the county total *doesn't equal* the natinal
 # cases reported? `num_county_diff`
-
+num_county_diff <- all_totals %>%
+  filter(county_total != cases)
+num_county_diff <- nrow(num_county_diff)
 
 # Oh no! An inconsistency -- let's dig further into this. Let's see if we can
 # find out *where* this inconsistency lies. Let's take the county level data,
@@ -222,17 +290,20 @@
 # (To avoid DPLYR automatically grouping your results,
 # specify `.groups = "drop"` in your `summarize()` statement. This is a bit of
 # an odd behavior....)
-
+sum_county_to_state <- counties %>%
+  group_by(state, date) %>%
+  summarise(county_totals = sum(cases))
 
 # Then, let's join together the `sum_county_to_state` dataframe with the
 # `states` dataframe into the variable `joined_states`.
-
+joined_states <- left_join(sum_county_to_state, states, by="state")
 
 # To find out where (and when) there is a discrepancy in the number of cases,
 # create the variable `has_discrepancy`, which has *only* the observations
 # where the sum of the county cases in each state and the state values are
 # different. This will be a *dataframe*.
-
+has_discrepancy <- joined_states %>%
+  filter(cases != county_totals)
 
 # Next, lets find the *state* where there is the *highest absolute difference*
 # between the sum of the county cases and the reported state cases.
